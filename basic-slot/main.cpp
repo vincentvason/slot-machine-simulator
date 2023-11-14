800644b2e8dcc58e50a30a0f63dbeca8aa7b8fc6
Vason Maitree <vason.mi3@gmail.com>
2023-11-14 00:41:27 +0000

    Update RTP Simulator

--- basic-slot/main.cpp
+++ basic-slot/main.cpp
@@ -2,12 +2,13 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <cmath>
 
 // Symbol
 // Symbol [S]even, [B]ell, [3]Bar, [2]Bar, [1]Bar, [C]herry, [F]ree
-std::vector<char> r1{'S','S','S','B','B', 'B','B','3','3','3', '2','2','2','1','1', '1','1','C','C','C', 'C','F','F','F'};
-std::vector<char> r2{'S','S','S','B','B', 'B','3','3','3','2', '2','2','1','1','1', '1','1','C','C','C', 'C','F','F','F'};
-std::vector<char> r3{'S','S','B','B','B', '3','3','3','2','2', '2','2','1','1','1', '1','C','C','C','C', 'C','F','F','F'};
+std::vector<char> r1{'S','S','S','S','S', 'B','B','B','B','3', '3','3','2','2','2', '1','1','1','C','C', 'C','C','C','F'};
+std::vector<char> r2{'S','S','S','B','B', 'B','3','3','3','2', '2','2','2','1','1', '1','1','1','C','C', 'C','C','C','C'};
+std::vector<char> r3{'S','B','B','3','3', '3','3','2','2','2', '2','1','1','1','1', '1','C','C','C','C', 'C','C','C','C'};
 
 int main()
 {
@@ -15,7 +16,7 @@ int main()
     float credits_wins = 0;
     int free_spin_wins = 0;
 
-    std::string symbol[10] = {"Free Game", "Seven", "Bell", "3-Bar", "2-Bar", "1-Bar", "3 Cheery", "Any Bar", "2 Cheery", "1 Cheery"};
+    std::string symbol[10] = {"Free Game", "Seven", "Bell", "3-Bar", "2-Bar", "1-Bar", "3 Cheery", "2 Cheery", "Any Bar", "1 Cheery"};
     int win[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 
@@ -24,7 +25,7 @@ int main()
         for (char r2i : r2)
             for(char r3i : r3)
                 {
-                    if(r1i == 'F' && r2i == 'F' && r3i == 'F')
+                    if(r1i == 'F')
                     {
                         ++free_spin_wins;
                         ++win[0];
@@ -41,32 +42,32 @@ int main()
                     }
                     else if(r1i == '3' && r2i == '3' && r3i == '3')
                     {
-                        credits_wins += 90.0/3.0;
+                        credits_wins += 60.0/3.0;
                         ++win[3];
                     }
                     else if(r1i == '2' && r2i == '2' && r3i == '2')
                     {
-                        credits_wins += 60.0/3.0;
+                        credits_wins += 45.0/3.0;
                         ++win[4];
                     }
                     else if(r1i == '1' && r2i == '1' && r3i == '1')
                     {
-                        credits_wins += 45.0/3.0;
+                        credits_wins += 30.0/3.0;
                         ++win[5];
                     }
                     else if(r1i == 'C' && r2i == 'C' && r3i == 'C')
                     {
-                        credits_wins += 15.0/3.0;
+                        credits_wins += 6.0/3.0;
                         ++win[6];
-                    }
-                    else if(isdigit(r1i) && isdigit(r2i) && isdigit(r3i))
-                    {
-                        credits_wins += 3.0/3.0;
-                        ++win[7];
                     }
                     else if(r1i == 'C' && r2i == 'C')
                     {
                         credits_wins += 3.0/3.0;
+                        ++win[7];
+                    }
+                    else if(isdigit(r1i) && isdigit(r2i) && isdigit(r3i))
+                    {
+                        credits_wins += 1.0/3.0;
                         ++win[8];
                     }
                     else if(r1i == 'C')
@@ -88,17 +89,17 @@ int main()
     std::cout << "RTP /wo Free Game: \t" << credits_wins/(spins - free_spin_wins) << std::endl;
 
     float rtp_no_free_spin = credits_wins/(spins - free_spin_wins);
-    float free_spin_probability = free_spin_wins/spins;
+    float free_spin_probability = static_cast<float>(free_spin_wins)/spins;
 
     /* Calculate Free Games RTP */
     float retrigger_chance = 1;
 
-    for( int i = 1; i <= 10; i++)
+    for( int i = 1; i <= 100; i++)
     {
-        retrigger_chance *= 1 + free_spin_probability;
+        retrigger_chance += pow(5 * free_spin_probability, i);
     }
 
-    float rtp_free_spin = (rtp_no_free_spin * 8 * retrigger_chance);
+    float rtp_free_spin = (rtp_no_free_spin * 5 * (retrigger_chance));
     std::cout << "Free Game Play: \t" << free_spin_wins << std::endl;
     std::cout << "RTP on Free Game: \t" << rtp_free_spin << std::endl;
 
--- basic-slot/simulation.cpp
+++ basic-slot/simulation.cpp
@@ -3,35 +3,62 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <algorithm>
+#include <ctime>
 
 // Symbol
 // Symbol [S]even, [B]ell, [3]Bar, [2]Bar, [1]Bar, [C]herry, [F]ree
-std::vector<char> r1{'S','S','S','B','B', 'B','B','3','3','3', '2','2','2','1','1', '1','1','C','C','C', 'C','F','F','F'};
-std::vector<char> r2{'S','S','S','B','B', 'B','3','3','3','2', '2','2','1','1','1', '1','1','C','C','C', 'C','F','F','F'};
-std::vector<char> r3{'S','S','B','B','B', '3','3','3','2','2', '2','2','1','1','1', '1','C','C','C','C', 'C','F','F','F'};
+std::vector<char> r1{'S','S','S','S','S', 'B','B','B','B','3', '3','3','2','2','2', '1','1','1','C','C', 'C','C','C','F'};
+std::vector<char> r2{'S','S','S','B','B', 'B','3','3','3','2', '2','2','2','1','1', '1','1','1','C','C', 'C','C','C','C'};
+std::vector<char> r3{'S','B','B','3','3', '3','3','2','2','2', '2','1','1','1','1', '1','C','C','C','C', 'C','C','C','C'};
+
+float simulation();
 
 int main()
 {
-    int simulation_count = 10000000;
+    float rtp;
+    float average_rtp;
+
+    for(int i = 0; i < 1000; i++)
+    {
+        rtp = simulation();
+        std::cout << "RTP @ " << i << " :" << rtp << std::endl;
+        
+        average_rtp = ((average_rtp*i) + rtp)/(i+1);
+    }
+
+    std::cout << "Average RTP :" << average_rtp << std::endl;
+    return 0;
+}
+
+float simulation()
+{
+    time_t timer;
+    int simulation_count = 1000000;
 
     int spins = 0;
     float credits_wins = 0;
     int free_spin_played = 0;
 
-    std::string symbol[10] = {"Free Game", "Seven", "Bell", "3-Bar", "2-Bar", "1-Bar", "3 Cheery", "Any Bar", "2 Cheery", "1 Cheery"};
+    std::string symbol[10] = {"Free Game", "Seven", "Bell", "3-Bar", "2-Bar", "1-Bar", "3 Cheery", "2 Cheery", "Any Bar", "1 Cheery"};
     int win[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    int r1i, r2i, r3i;
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(r1), std::end(r1), rng);
+    std::shuffle(std::begin(r2), std::end(r2), rng);
+    std::shuffle(std::begin(r3), std::end(r3), rng);
 
+    int r1i, r2i, r3i;
+    srand(time(&timer));   
     for(int i = 0; i < simulation_count + free_spin_played; i++)
     {
         r1i = r1[rand() % 24];
         r2i = r2[rand() % 24];
         r3i = r3[rand() % 24];
 
-        if(r1i == 'F' && r2i == 'F' && r3i == 'F')
+        if(r1i == 'F')
         {
-            free_spin_played += 8;
+            free_spin_played += 5;
             ++win[0];
         }
         else if(r1i == 'S' && r2i == 'S' && r3i == 'S')
@@ -46,32 +73,32 @@ int main()
         }
         else if(r1i == '3' && r2i == '3' && r3i == '3')
         {
-            credits_wins += 90.0/3.0;
+            credits_wins += 60.0/3.0;
             ++win[3];
         }
         else if(r1i == '2' && r2i == '2' && r3i == '2')
         {
-            credits_wins += 60.0/3.0;
+            credits_wins += 45.0/3.0;
             ++win[4];
         }
         else if(r1i == '1' && r2i == '1' && r3i == '1')
         {
-            credits_wins += 45.0/3.0;
+            credits_wins += 30.0/3.0;
             ++win[5];
         }
         else if(r1i == 'C' && r2i == 'C' && r3i == 'C')
         {
-            credits_wins += 15.0/3.0;
+            credits_wins += 6.0/3.0;
             ++win[6];
-        }
-        else if(isdigit(r1i) && isdigit(r2i) && isdigit(r3i))
-        {
-            credits_wins += 3.0/3.0;
-            ++win[7];
         }
         else if(r1i == 'C' && r2i == 'C')
         {
             credits_wins += 3.0/3.0;
+            ++win[7];
+        }
+        else if(isdigit(r1i) && isdigit(r2i) && isdigit(r3i))
+        {
+            credits_wins += 1.0/3.0;
             ++win[8];
         }
         else if(r1i == 'C')
@@ -79,16 +106,9 @@ int main()
             credits_wins += 1.0/3.0;
             ++win[9];
         }
-        ++spins;
 
-        if(i % (simulation_count/20) == 0){
-            std::cout << (i / (simulation_count/20)) * 5 << "%" << std::endl; 
-        }
+        ++spins;
     }
 
-    std::cout << "Spins: \t" << spins - free_spin_played << std::endl;    
-    std::cout << "Free Games: \t" << free_spin_played << std::endl;
-    std::cout << "Credits Win: \t" << credits_wins << std::endl;
-    std::cout << "RTP: \t" << credits_wins/(spins - free_spin_played) << std::endl;
-     
+    return credits_wins/(spins - free_spin_played);
 }
